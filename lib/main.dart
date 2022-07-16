8b6734ba0584d63bd1061a78c9c494a5e36e5b64
sajibsaha4647 <sksaha4648@gmail.com>
2022-07-16 15:31:09 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:liquid_swipe/liquid_swipe.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -18,13 +18,42 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+
+    var pages = [
+      Container(color: Colors.amberAccent,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Text("sajib saha"),
+      ),
+      Container(color: Colors.green,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Text("sajib saha"),
+      ),
+      Container(color: Colors.red,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Text("sajib saha"),
+      ),
+      Container(color: Colors.purple,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Text("sajib saha"),
+      ),
+    ];
+
+
     return SafeArea(
         child: Scaffold(
       body: Container(
-
+          child: LiquidSwipe(
+              pages: pages
+          )),
       ),
-    ));
+    );
   }
 }
